4b74908db5b8d5fe7e162ee8855df7804cf05915
katiewimmer <71279410+katiewimmer@users.noreply.github.com>
2020-10-15 22:29:41 -0400

    start and stop timer

--- dashboard/main.py
+++ dashboard/main.py
@@ -1,1 +1,19 @@
 
+class Dashboard:
+
+    bool time = False;
+  #  bool sched = False;
+  #  bool task = False;
+
+    def start_timer(max_count):
+        self.time = True
+        if getTime() >= max_count:
+            self.time = False
+    def stop_time():
+        self.time = False
+    
+  #  def open_schdule()
+    #    self.sched = True
+    
+
+        
