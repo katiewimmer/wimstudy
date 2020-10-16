c91312da8c7227d483c7ec5d167552ebdf9f0bf2
katiewimmer <71279410+katiewimmer@users.noreply.github.com>
2020-10-16 00:50:11 -0400

    first constructor/method

--- dashboard/timer/timer.py
+++ dashboard/timer/timer.py
@@ -1,1 +1,16 @@
 
+class Timer:
+
+int count = 0;
+
+# USED TO SET + RETURN CURRENT TIMER VALUE
+    def __init__ (number = 0):
+        self.count = number
+
+    def display_count():
+        print(self.count)
+
+
+
+    
+
