b003a9cda0d09de9a0658b8bfb5ffc55c5b81674
katiewimmer <71279410+katiewimmer@users.noreply.github.com>
2020-10-22 10:44:03 -0400

    added constructor

--- dashboard/task_list/task_list.py
+++ dashboard/task_list/task_list.py
@@ -1,1 +1,29 @@
 
+
+
+def __init__(self, name, time, category):
+    self.name = name
+    self.time = time
+    self.category = category
+
+def display_full_task(self)
+    print ("Name: " + str(self.name) + /n)
+    print ("Time: " + str(self.time) + /n)
+    print ("Category: " + str(self.category) + /n)
+
+def get_name():
+    return self.name
+def get_time():
+    return self.time
+def get_catefory:
+    return self.category
+
+def make_task_list(*task):
+    
+def get_tasks
+    print("The available tasks are:")
+    print(tasks)
+
+
+
+
