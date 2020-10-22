181952d4b3364800cf442fa01a6a9b23145b0915
katiewimmer <71279410+katiewimmer@users.noreply.github.com>
2020-10-21 21:05:49 -0400

    greeting sequence

--- dashboard/main.py
+++ dashboard/main.py
@@ -1,22 +1,27 @@
 
-class Dashboard:
+def run_dashboard():
+  '''
+  Will open and display the three tools
+  '''
+  greeting()
 
-    bool time = False;
-  #  bool sched = False;
-  #  bool task = False;
+def greeting():
+  
+  mood = input ("How productive are you feeling today? On a scale of 1-10: /n")
 
-  # CODE FOR DISPLAY
+  if mood > 0
+    time = input ("How much time do you have? In minutes: /n")
+  
+  if time > 0
+    print("Please choose a task: /n")
+    # show_tasks()
+  
+  
+#def show_tasks():
+   
+#   get_tasks()
 
-    # WILL START/STOP TIMER THROUGH DISPLAY
-    def start_timer(max_count):
-        self.time = True
-        if getTime() >= max_count:
-            self.time = False
-    def stop_time():
-        self.time = False
-    
-  #  def open_schdule()
-    #    self.sched = True
-    
+if __name__ == "__main__":
+    run_dashboard()
 
-        
+  
