b98e983fddfb7c25009558ab0a9f3f844c2dcc75
Sima Shmuylovich <sshmuylovich21@andover.edu>
2020-10-23 01:29:14 -0400

    fixed colons, indents, and task_list so it can run

--- dashboard/main.py
+++ dashboard/main.py
@@ -7,40 +7,46 @@ def run_dashboard():
 
 def greeting():
   
-  mood = input ("How productive are you feeling today? On a scale of 1-10: /n")
+  mood = input ("How productive are you feeling today? On a scale of 1-10: \n")
 
-  if mood > 0
-    time = input ("How much time do you have? In minutes: /n")
+  if int(mood) in range(0,10):
+    time = int(input ("How much time do you have? In minutes: \n"))
+    if time > 0:
+      print("Please choose a task: \n")
+      # show_tasks()
+  else:
+    print("There was an error with your input. Please try again!\n")
+    greeting()
   
-  if time > 0
-    print("Please choose a task: /n")
-    # show_tasks()
   
   def get_action():
 
-    need_task = input("Do you want to select a specific task? /n")
+    need_task = input("Do you want to select a specific task? \n")
 
     if need_task.lower() in ['n', 'no']:
-        print("Fantastic, lets get started!")
+        print("Fantastic, lets get started!\n")
         study()
         exit()
-      elif need.task.lower() in ['y', 'yes']:
+    elif need_task.lower() in ['y', 'yes']:
         recommend_task()
     else:
-        print("Please answer yes or no")
+        print("Please answer yes or no.\n")
         get_action()
-  def recommend_task
-  '''
-    Need to print the tasks with numbers than allow the user to select a 
-    specific task by entering a number
-  '''
-    for x in task_list
+  
+
+  def recommend_task():
+    '''
+      Need to print the tasks with numbers than allow the user to select a 
+      specific task by entering a number
+    '''
+    for x in task_list():
       display_full_task(x)
 
 #    task_num = input(f' "Please enter the number of the task you would like: /n {get_tasks()}'')
 #    for x in tasks
 
 def study():
+  return #spacer
   
 #def show_tasks():
    
