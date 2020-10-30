2bb9ad189f5abfde02eba6c4b026300b4c25879e
sshmuylovich <71236435+sshmuylovich@users.noreply.github.com>
2020-10-30 04:06:53 -0400

    Update wimstudy_gui.py

--- dashboard/wimstudy_gui.py
+++ dashboard/wimstudy_gui.py
@@ -1,1 +1,15 @@
 
+import tkinter as tk
+from tkinter import *
+
+
+HEIGHT = 800
+WIDTH = 700
+
+root = tk.Tk()
+
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg='white')
+canvas.pack()
+
+root.mainloop()
