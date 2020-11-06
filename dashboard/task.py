4398e7a3aceb01606d78820ca49d2385117ebe86
katiewimmer <71279410+katiewimmer@users.noreply.github.com>
2020-11-06 00:08:05 -0500

    add __init__ def

--- dashboard/task.py
+++ dashboard/task.py
@@ -1,1 +1,21 @@
 
+class task:
+    def __init__(self,name,time,category):
+        self.name=name
+        self.time=time
+        self.category=category
+
+    def display_full_task(self):
+        print("Name:",str(self.name),"\n")
+        print("Time:",str(self.time),"\n")
+        print("Category:",str(self.category),"\n")
+
+    def get_name(self):
+        return self.name
+
+    def get_time(self):
+        return self.time
+
+    def get_category(self):
+        return self.category
+
