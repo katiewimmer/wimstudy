f10352fdc6ce7dcdbf706f97a8d2a916e6fda6ae
katiewimmer <71279410+katiewimmer@users.noreply.github.com>
2020-11-12 13:14:29 -0500

    delete rec function

--- dashboard/task_list.py
+++ dashboard/task_list.py
@@ -1,21 +1,3 @@
-def print_tasks():
-    print("The available tasks are:")
-    for t in list_of_tasks:
-         print(display_full_task(t))
-        
-'''
-for x in range(0, num_tasks):
-    n = input("\ninput name: \n") #Customize Questions
-    t = input("input time: \n")
-    c = input("input category: \n")
-
-    task=task_list(n,t,c)
-
-    list_of_tasks.insert(x, task.name)
-
-print("\nList of Tasks:", list_of_tasks)
-'''
-## Additional methods that don't have to do with the code when you run it as of now
 
 def recommend_task(mood, time, task_list):
     for i, x in enumerate(0, len(task_list)):
